41ed3eb8620a0b227fd4a44ef3848d8d19e0dc39
kurniawan <mrsin178@gmail.com>
2021-12-20 16:55:26 +0700

    login

---
+++ dashboard/login.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/app/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/icons/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./dist/css/login.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+<section id="kontak" class="kontak text-center">
+  <div class="container">
+    <div class="row">
+    </div>
+    <div class="row justify-content-center text-start">
+      <div class="col-md-6">
+      <form>
+    <div class="logo text-center">
+        <h1>MyCreep</h1>
+    </div>
+  <div class="mb-3">
+    <label for="name" class="form-label">Username</label>
+    <input type="text" class="form-control" id="name" aria-describedby="name">
+  </div>
+
+  <div class="mb-3">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" id="password" aria-describedby="password">
+  </div>
+
+<div class="submit text-center">
+  <button type="submit" class="btn">Submit</button>
+  </div>
+</form>
+      </div>
+    </div>
+  </div>
+    </section>
+
+    <div class="slider-background" id="sliders-background"></div>
+    <script src="./dist/js/jquery.js"></script>
+    <script src="./assets/app/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+
+</body>
+
+</html>
