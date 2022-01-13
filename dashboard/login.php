2a14a69f736a5d6ba7fbcfe92ea34230171d2f9d
kurniawan <mrsin178@gmail.com>
2022-01-13 18:23:54 +0700

    Update Login

--- dashboard/login.php
+++ dashboard/login.php
@@ -21,24 +21,46 @@
     </div>
     <div class="row justify-content-center text-start">
       <div class="col-md-6">
-      <form>
+        
+      <form action="" method="POST">
     <div class="logo text-center">
         <h1>MyCreep</h1>
     </div>
   <div class="mb-3">
-    <label for="name" class="form-label">Username</label>
-    <input type="text" class="form-control" id="name" aria-describedby="name">
+    <label for="user" class="form-label">Username</label>
+    <input type="text" class="form-control" name="user" id="user" aria-describedby="name">
   </div>
 
   <div class="mb-3">
-    <label for="password" class="form-label">Password</label>
-    <input type="password" class="form-control" id="password" aria-describedby="password">
+    <label for="pass" class="form-label">Password</label>
+    <input type="password" class="form-control" name="pass" id="pass" aria-describedby="password">
   </div>
 
 <div class="submit text-center">
-  <button type="submit" class="btn">Submit</button>
+  <button type="submit" name="submit" class="btn">Submit</button>
   </div>
 </form>
+<?php
+           if(isset($_POST['submit'])){
+               include '../config.php';
+
+                $user = $_POST['user'];
+                $pass = $_POST['pass'];
+
+                $cek = mysqli_query($conn, "SELECT * FROM tb_admin WHERE username = '".$user."' AND password = '".MD5($pass)."'");
+                if(mysqli_num_rows($cek) > 0 ){
+
+                    $d = mysqli_fetch_object($cek);
+                    $_SESSION['status_login'] = true;
+                    $_SESSION['admin_global'] = $d;
+                    $_SESSION['id'] = $d -> admin_id;
+
+                    echo '<script>window.location="index.php"</script>';
+                }else{
+                    echo '<script>alert("Username atau Password Anda Salah!")</script>';
+                }
+            }
+        ?>
       </div>
     </div>
   </div>
